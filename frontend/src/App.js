869cee79bf73e08a0a473e015d0d29e0c671fcbf
Yogurt7v <egorov-2k@yandex.ru>
2024-10-28 20:30:36 +0300

    add cicd

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Test to Ci/Cd with Create React App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
